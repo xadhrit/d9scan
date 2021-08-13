79d89c70cfb0c161a0dee580d19508a32ba52dbd
xadhrit <maharajrabel@gmail.com>
2021-08-13 12:04:27 -0400

    add: help usage

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 if [[ "$OSTYPE"  == "linux-gnu" || "darwin"  ]]; 
 then
    echo -e "Donwloading requirements\n"
-   python3 -m pip  install -r  requirements.txt
+   python3 -m pip  install -r  requirements.txt && python3 scan.py -h
      
 else
    echo -e "Donwloading requirements\n"
-   python -m pip  install -r requirements.txt
+   python -m pip  install -r requirements.txt && python scan.py -h
   
 fi
 
