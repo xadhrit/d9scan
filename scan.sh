dfc35f278915b701ca96813cb5428dcdbd9efde4
xadhrit <maharajrabel@gmail.com>
2021-08-13 22:10:53 -0400

    scanning colored

--- scan.sh
+++ scan.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#nmap -sV --script=http-malware-host {ip}
-#nmap -sV --script http-dlink-backdoor {ip}
-#nmap --script ftp-proftpd-backdoor -p 21 {ip}
+CYAN="\e[36m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
-printf "d9scan from shell :) \n\n"
+printf "${CYAN} d9scan from shell :) \n\n"
 printf "Enter target's domain or IP : "
 read IP 
 
 function options(){
-   printf "----------------- Choose One option ----------------------- "
+   printf "----------------- Choose One option -----------------------${GREEN} "
    echo -e  " 1 for http-malware-host scan"
    echo -e  " 2 for http-dlink-backdoor scan"
    echo -e  " 3 for ftp-proftpd-backdoor scan"
@@ -20,15 +20,19 @@ function options(){
    
    if [[ $OPTION == '1' ]] ;
    then
+       echo -e "${GREEN} nmap -sv --script=http-malware-host ${IP} \n"
        nmap -sV --script=http-malware-host $IP
    elif [[ $OPTION == '2'  ]] ;
    then
+      echo -e "${GREEN} nmap -sV --script http-dlink-backdoor ${IP}\n "
       nmap -sV --script http-dlink-backdoor $IP
    elif [[ $OPTION == '3'  ]];
    then
+      echo -e "${GREEN} nmap --script ftp-proftpd-backdoor -p 21 ${IP}\n "
       nmap --script ftp-proftpd-backdoor -p 21 $IP
    elif [[ $OPTION == '4'  ]] ;
    then
+       echo -e "${CYAN} Running all Scripts\n "
        nmap -sV --script=http-malware-host $IP
        nmap -sV --script http-dlink-backdoor $IP
        nmap --script ftp-proftpd-backdoor -p 21 $IP
@@ -42,6 +46,7 @@ function options(){
    fi
 
 }
+"${GREEN} nmap -v -A ${IP} \n"
 nmap -v -A $IP
 options
 exit 
